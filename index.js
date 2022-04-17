bee12ccb36ae2047450f53131dd964a093cfe98a
jamarv4 <23177114+jamarv4@users.noreply.github.com>
2022-04-17 14:14:29 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+/**
+ * WorldHax
+ * Trainer to hack WorldBox: God Simulator
+ * 
+ * Converting pointers from CheatEngine Tables into
+ * an executable that allows us to modify game memory, thus hacking it
+ */
+
